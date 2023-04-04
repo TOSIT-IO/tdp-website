2e1deec8e58c4bcd55c5ec9c038f0b47d0b19eca
David Worms <david@adaltas.com>
2023-04-04 18:25:25 +0200

    refactor: next layout like a pro

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,5 @@
 import clsx from 'clsx'
-import engine from '/src/engine/index.js'
 import { Inter, Fira_Code } from 'next/font/google'
-import Footer from './layout/footer'
-import Header from './layout/header'
-import Left from './layout/left'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
@@ -20,90 +16,19 @@ export const metadata = {
   },
 }
 
-export default async function RootLayout ({ children }) {
-  return <>{children}</>
-  // console.log('!!!params', params)
-  // const db = engine('./content')
-  // const menuTop = await db
-  //   .from('pages')
-  //   .filter(
-  //     (page) =>
-  //       page.slug.length === 1 &&
-  //       (page.slug[0] !== 'instructions' ||
-  //         process.env.NODE_ENV === 'development')
-  //   )
-  //   .map((page) => ({
-  //     lang: page.lang,
-  //     title: page.data.nav_title || page.data.title,
-  //     slug: page.slug,
-  //   }))
-  // const menuLeft = await db
-  //   .from('pages')
-  //   .filter((page) => page.lang === 'en' && page.slug[0] === 'docs')
-  //   .map(page => ({
-  //     lang: page.lang,
-  //     title: page.data.nav_title || page.data.title,
-  //     section: page.data.section,
-  //     slug: page.slug,
-  //   }))
-  //   .tree()
-  //   .get()
-  // return (
-  //   <html lang="en">
-  //     <body
-  //       className={clsx(
-  //         'relative text-slate-200',
-  //         inter.variable, 'font-sans', fira.variable,
-  //       )}
-  //     >
-  //       <Header
-  //         menuTop={menuTop}
-  //         className={clsx(
-  //           'fixed top-0 w-full z-10 h-[60px] border-b border-slate-500',
-  //           'flex',
-  //         )}
-  //         style={{
-  //           background: `radial-gradient(50% 50% at 50% 50%, rgba(14, 11, 22, 0.12) 0%, rgba(0, 0, 0, 0) 100%), radial-gradient(17.86% 94.3% at 87.98% 36.67%, rgba(27, 83, 83, 0.18) 0%, rgba(0, 0, 0, 0) 100%), radial-gradient(50.96% 97.73% at 18.2% 68.08%, rgba(28, 74, 74, 0.26) 0%, rgba(0, 0, 0, 0) 100%), rgba(44, 48, 49, 0.90)`,
-  //         }}
-  //       />
-  //       <div
-  //         className="w-full h-full flex min-h-screen"
-  //         style={{
-  //           background: `radial-gradient(23.91% 23.92% at 14.33% 30.99%, rgba(19, 10, 39, 0.2) 0%, rgba(32, 10, 39, 0) 100%), radial-gradient(37.93% 37.93% at 85.72% 23.89%, rgba(4, 11, 18, 0.2) 0%, rgba(58, 95, 129, 0) 100%), radial-gradient(25.52% 25.52% at 24.55% 76.74%, rgba(95, 116, 97, 0.2) 0%, rgba(45, 67, 48, 0) 100%), radial-gradient(42.95% 42.95% at 21.12% 25.63%, rgba(97, 130, 120, 0.2) 0%, rgba(122, 159, 132, 0) 100%), radial-gradient(63.47% 63.46% at 31.32% 63.18%, rgba(64, 86, 142, 0.2) 0%, rgba(0, 0, 0, 0) 100%), radial-gradient(19.87% 43.71% at 73.92% 40.01%, rgba(122, 159, 132, 0.116) 0%, rgba(40, 52, 59, 0.056) 100%), #263134`,
-  //         }}
-  //       >
-  //         <div
-  //           className={clsx(
-  //             "pt-[8rem] pb-5",
-  //             "w-[400px] [&>*]:w-[300px] [&>*]:ml-[80px] [&>*]:mr-[20px]",
-  //             "border-r border-r-white/40",
-  //           )}
-  //           style={{
-  //             background: `rgba(0, 0, 0, 0.13)`,
-  //           }}
-  //         >
-  //           <Left
-  //             className={clsx(
-  //               "sticky top-20",
-  //             )}
-  //             menuLeft={menuLeft}
-  //           />
-  //         </div>
-  //         <div className="grow relative">
-  //           <main className="max-w-4xl m-auto py-40">
-  //             {children}
-  //           </main>
-  //         </div>
-  //       </div>
-  //       <div
-  //         className="p-10 border-t-2 border-slate-500"
-  //         style={{
-  //           background: `linear-gradient(154.09deg, rgba(0, 0, 0, 0.2) 16.35%, rgba(0, 0, 0, 0) 39.32%), radial-gradient(14.08% 17.22% at 86.26% 77.02%, rgba(255, 255, 255, 0.04) 0%, rgba(255, 255, 255, 0) 100%), radial-gradient(31.27% 31.27% at 81.13% 24.31%, rgba(86, 80, 28, 0.2) 0%, rgba(11, 36, 32, 0) 100%), radial-gradient(26.56% 26.56% at 19.78% 70.31%, rgba(54, 22, 28, 0.2) 0%, rgba(10, 14, 27, 0) 100%), linear-gradient(180deg, #2F3A44 0%, #202B2B 100%)`,
-  //         }}
-  //       >
-  //         <Footer />
-  //       </div>
-  //     </body>
-  //   </html>
-  // )
+export default async function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={clsx(
+          'relative text-slate-200 dark',
+          inter.variable,
+          'font-sans',
+          fira.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  )
 }
