2099642fcda8b782d2168c8027f222d5cce56620
David Worms <david@adaltas.com>
2023-04-05 22:37:55 +0200

    feat: spin-off tabs from code

--- src/mdx/components/index.js
+++ src/mdx/components/index.js
@@ -1,4 +1,4 @@
-export { CodeGroup } from '@/mdx/components/Code'
+// export { CodeGroup } from '@/mdx/components/Code'
 export { Code as code, Pre as pre } from '@/mdx/components/Code'
-// export {  } from '@/mdx/components/Tabs'
+export { Tabs } from '@/mdx/components/Tabs'
 export { Properties, Property } from '@/mdx/components/Properties'
