70627cf16df6344a053cc0b8d85a617d170a50ce
David Worms <david@adaltas.com>
2023-04-06 22:32:07 +0200

    feat: active current menu pag

--- src/app/[lang]/layout.js
+++ src/app/[lang]/layout.js
@@ -1,42 +1,9 @@
-import clsx from 'clsx'
-import engine from '@/engine/index.js'
 import Footer from '../layout/footer/index.jsx'
-import Header from '../layout/header'
 
 export default async function RootLayout({ children }) {
-  const menuTop = await engine('./content')
-    .from('pages')
-    .filter(
-      (page) =>
-        page.slug.length === 1 &&
-        (page.slug[0] !== 'instructions' ||
-          process.env.NODE_ENV === 'development')
-    )
-    .map((page) => ({
-      lang: page.lang,
-      title: page.data.nav_title || page.data.title,
-      slug: page.slug,
-    }))
   return (
     <>
-      <Header
-        menuTop={menuTop}
-        className={clsx(
-          'fixed top-0 w-full z-10 h-[60px] border-b border-slate-500',
-          'flex'
-        )}
-        style={{
-          background: `radial-gradient(50% 50% at 50% 50%, rgba(14, 11, 22, 0.12) 0%, rgba(0, 0, 0, 0) 100%), radial-gradient(17.86% 94.3% at 87.98% 36.67%, rgba(27, 83, 83, 0.18) 0%, rgba(0, 0, 0, 0) 100%), radial-gradient(50.96% 97.73% at 18.2% 68.08%, rgba(28, 74, 74, 0.26) 0%, rgba(0, 0, 0, 0) 100%), rgba(44, 48, 49, 0.90)`,
-        }}
-      />
-      <div
-        className="w-full h-full flex min-h-screen"
-        style={{
-          background: `radial-gradient(23.91% 23.92% at 14.33% 30.99%, rgba(19, 10, 39, 0.2) 0%, rgba(32, 10, 39, 0) 100%), radial-gradient(37.93% 37.93% at 85.72% 23.89%, rgba(4, 11, 18, 0.2) 0%, rgba(58, 95, 129, 0) 100%), radial-gradient(25.52% 25.52% at 24.55% 76.74%, rgba(95, 116, 97, 0.2) 0%, rgba(45, 67, 48, 0) 100%), radial-gradient(42.95% 42.95% at 21.12% 25.63%, rgba(97, 130, 120, 0.2) 0%, rgba(122, 159, 132, 0) 100%), radial-gradient(63.47% 63.46% at 31.32% 63.18%, rgba(64, 86, 142, 0.2) 0%, rgba(0, 0, 0, 0) 100%), radial-gradient(19.87% 43.71% at 73.92% 40.01%, rgba(122, 159, 132, 0.116) 0%, rgba(40, 52, 59, 0.056) 100%), #263134`,
-        }}
-      >
-        {children}
-      </div>
+      {children}
       <div
         className="p-10 border-t-2 border-slate-500"
         style={{
--- src/app/layout/left/index.jsx
+++ src/app/layout/left/index.jsx
@@ -1,9 +1,129 @@
-
 import clsx from 'clsx'
-import { LiSection, LiChildOfSection, Li, Ul } from './MenuItems'
+import Link from 'next/link'
+
+export const Ul = function ({
+  current,
+  level,
+  pages,
+  parentIsSection,
+}) {
+  return (
+    <ul>
+      {pages.map((page, i) => (
+        page.section
+          ? <LiSection
+            current={current}
+            key={page.slug.join('/')}
+            page={page}
+            level={level}
+            last={i === pages.length - 1}
+          />
+          : parentIsSection
+            ? <LiChildOfSection
+              current={current}
+              key={page.slug.join('/')}
+              page={page}
+              level={level}
+              last={i === pages.length - 1}
+            />
+            : <Li
+              current={current}
+              key={page.slug.join('/')}
+              page={page}
+              level={level}
+              last={i === pages.length - 1}
+            />
+      ))}
+    </ul>
+  )
+}
+
+export const LiSection = function ({
+  current,
+  level,
+  last,
+  page,
+}) {
+  return (
+    <li key={page.slug.join('/')}>
+      <h2 className="italic font-extralight text-white/50 py-4">
+        {page.title}
+      </h2>
+      <Ul current={current} pages={page.children} level={level+1} parentIsSection={true} />
+    </li>
+  )
+}
+
+export const LiChildOfSection = function ({
+  current,
+  level,
+  last,
+  page,
+}) {
+  const href = `/${page.lang}/${page.slug.join('/')}`
+  return (
+    <li key={page.slug.join('/')}>
+      <div className="relative pb-2">
+        {last ||
+          <span className="absolute left-1 top-2 -ml-px h-full w-0.5 bg-white/40" aria-hidden="true" />
+        }
+        <div className="relative flex space-x-3">
+          <div className="mt-2">
+            <span
+              className={clsx(
+                'bg-[#00FFFA]',
+                'h-2 w-2 rounded-full flex items-center justify-center ring-2 ring-black/40'
+              )}
+            />
+          </div>
+          <div className="space-y-1">
+            <Link
+              className={clsx(
+                "py-4",
+                JSON.stringify(current) === JSON.stringify(page.slug)
+                ? "text-white"
+                : "text-white/70 hover:text-[#00FFFA]"
+              )}
+              href={href}
+            >
+              {page.title}
+            </Link>
+            <Ul current={current} pages={page.children} level={level+1} />
+          </div>
+        </div>
+      </div>
+    </li>
+  )
+}
+
+export const Li = function ({
+  current,
+  level,
+  last,
+  page,
+}) {
+  const href = `/${page.lang}/${page.slug.join('/')}`
+  return (
+    <li>
+      <Link
+        className={clsx(
+          "py-2",
+          JSON.stringify(current) === JSON.stringify(page.slug)
+          ? "text-white"
+          : "text-white/70 hover:text-[#00FFFA]"
+        )}
+        href={`/${page.lang}/${page.slug.join('/')}`}
+      >
+        {page.title}
+      </Link>
+      <Ul current={current} pages={page.children} level={level+1} />
+    </li>
+  )
+}
 
 export default function Left({
   className,
+  current,
   menuLeft:page,
   level=0,
   style,
@@ -17,7 +137,7 @@ export default function Left({
       style={style}
     >
       <h1 className="sr-only">Menu for {page.title}</h1>
-      <Ul pages={pages} level={level+1} />
+      <Ul current={current} pages={pages} level={level+1} />
     </aside>
   )
 }
