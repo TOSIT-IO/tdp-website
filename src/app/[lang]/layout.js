ea0c48e1ae95d75e59b4f04f377d9193846fcb60
David Worms <david@adaltas.com>
2023-04-11 14:43:33 +0200

    feat: footer

--- src/app/[lang]/layout.js
+++ src/app/[lang]/layout.js
@@ -1,7 +1,7 @@
 import 'server-only'
 import Footer from '../layout/footer/index.jsx'
 
-export default async function RootLayout({ children }) {
+export default async function RootLayout({ children, params }) {
   return (
     <>
       {children}
@@ -11,7 +11,7 @@ export default async function RootLayout({ children }) {
           background: `linear-gradient(154.09deg, rgba(0, 0, 0, 0.2) 16.35%, rgba(0, 0, 0, 0) 39.32%), radial-gradient(14.08% 17.22% at 86.26% 77.02%, rgba(255, 255, 255, 0.04) 0%, rgba(255, 255, 255, 0) 100%), radial-gradient(31.27% 31.27% at 81.13% 24.31%, rgba(86, 80, 28, 0.2) 0%, rgba(11, 36, 32, 0) 100%), radial-gradient(26.56% 26.56% at 19.78% 70.31%, rgba(54, 22, 28, 0.2) 0%, rgba(10, 14, 27, 0) 100%), linear-gradient(180deg, #2F3A44 0%, #202B2B 100%)`,
         }}
       >
-        <Footer />
+        <Footer lang={params.lang} />
       </div>
     </>
   )
--- src/app/layout/footer/index.jsx
+++ src/app/layout/footer/index.jsx
@@ -1,17 +1,83 @@
 import 'server-only'
 import clsx from 'clsx'
+import Link from 'next/link'
 
 export default function Footer({
-  className
+  className,
+  lang,
+  page,
 }){
   return (
     <footer
       className={clsx(
         className,
-        'prose prose-invert',
       )}
     >
-      footer goes here
+      <ul className={clsx(
+        "max-w-4xl px-6 m-auto",
+        "grid grid-cols-3",
+        "[&_h2]:mb-3 [&_h2]:italic [&_h2]:font-extralight [&_h2]:text-white/50"
+      )}>
+        <li>
+          <h2>First step</h2>
+          <ul>
+            <li>
+              <Link
+                className="text-white/70 hover:text-[#00FFFA]"
+                href={`/${lang}/${['discover'].join('/')}`}
+              >
+                Discover
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-white/70 hover:text-[#00FFFA]"
+                href={`/${lang}/${['getting-started'].join('/')}`}
+              >
+                Getting started
+              </Link>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h2>Learn</h2>
+          <ul>
+            <li></li>
+          </ul>
+        </li>
+        <li>
+          <h2>Developper community</h2>
+          <ul className="space-y-1">
+            <li>
+              <a
+                className="text-white/70 hover:text-[#00FFFA]"
+                href="https://github.com/TOSIT-IO/TDP"
+                target="_blank"
+              >
+                GitHub repository
+              </a>
+            </li>
+            <li>
+              <a
+                className="text-white/70 hover:text-[#00FFFA]"
+                href="https://github.com/TOSIT-IO/TDP/issues"
+                target="_blank"
+              >
+                Issues
+              </a>
+            </li>
+            <li>
+              <a
+                className="text-white/70 hover:text-[#00FFFA]"
+                href="https://github.com/orgs/TOSIT-IO/discussions"
+                target="_blank"
+              >
+                Discussions
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
     </footer>
   )
 }
