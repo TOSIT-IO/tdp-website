453d351f7dfa4a15438f93bacb75ff2a14104915
David Worms <david@adaltas.com>
2023-05-23 12:50:52 +0200

    feat: print warning in case of heading 1 mis-usage

--- src/mdx/components.js
+++ src/mdx/components.js
@@ -1,11 +1,13 @@
 import { Code } from './components/Code.js'
 
 const Components = {
-  h1: ({ children }) => <div>ok</div>,
+  h1: () => (
+    <div>
+      WARNING: heading 1 are handled by the template, only one is accepted at
+      the begining of a page.
+    </div>
+  ),
   code: Code,
-  // h1: function(){
-  //   return <h1></h1>
-  // }
 }
 
 export default Components
