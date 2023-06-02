6c2f81780f0f8c748d9bc91a203ff6f75252f729
David Worms <david@adaltas.com>
2023-06-02 13:43:34 +0200

    build(project): enforce conventionnal commit

---
+++ commitlint.config.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-enum": [2, "always", ["content", "layout", "project"]],
+    "scope-empty": [2, "never"],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "refactor",
+        "revert",
+        "test",
+      ],
+    ],
+  },
+};
