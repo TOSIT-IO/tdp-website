7a576f8954d4581faaf384522ad656b8a3ea8d15
David Worms <david@adaltas.com>
2023-06-05 21:34:15 +0200

    feat(layout): border next to sidebar

--- src/app/[lang]/(pages)/[...slug]/layout/drawer/Sidebar.js
+++ src/app/[lang]/(pages)/[...slug]/layout/drawer/Sidebar.js
@@ -63,14 +63,15 @@ export default function Sidebar ({
               </Transition.Child>
               {/* Sidebar component, swap this element with another sidebar if you like */}
               <div
-                className="flex grow flex-col gap-y-5 overflow-y-auto px-6 pb-2"
+                className={clsx(
+                  "flex grow flex-col gap-y-5 overflow-y-auto px-6 pb-2",
+                  "border-r border-slate-500",
+                )}
                 style={{
                   background: `radial-gradient(23.91% 23.92% at 14.33% 30.99%, rgba(19, 10, 39, 0.2) 0%, rgba(32, 10, 39, 0) 100%), radial-gradient(37.93% 37.93% at 85.72% 23.89%, rgba(4, 11, 18, 0.2) 0%, rgba(58, 95, 129, 0) 100%), radial-gradient(25.52% 25.52% at 24.55% 76.74%, rgba(95, 116, 97, 0.2) 0%, rgba(45, 67, 48, 0) 100%), radial-gradient(42.95% 42.95% at 21.12% 25.63%, rgba(97, 130, 120, 0.2) 0%, rgba(122, 159, 132, 0) 100%), radial-gradient(63.47% 63.46% at 31.32% 63.18%, rgba(64, 86, 142, 0.2) 0%, rgba(0, 0, 0, 0) 100%), radial-gradient(19.87% 43.71% at 73.92% 40.01%, rgba(122, 159, 132, 0.116) 0%, rgba(40, 52, 59, 0.056) 100%), #263134`,
                 }}
               >
-                <div className="flex shrink-0 items-center">
-                  {children}
-                </div>
+                {children}
               </div>
             </Dialog.Panel>
           </Transition.Child>
