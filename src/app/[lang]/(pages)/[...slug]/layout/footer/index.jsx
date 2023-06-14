b0f38c297ed435c177d03cb56a1955a831945f18
David Worms <david@adaltas.com>
2023-06-14 23:05:07 +0200

    refactor(layout): move all footer code into its component

--- src/app/[lang]/(pages)/[...slug]/layout/footer/index.jsx
+++ src/app/[lang]/(pages)/[...slug]/layout/footer/index.jsx
@@ -23,132 +23,139 @@ export default async function Footer({
     .map(params => params.data)
     .get()
   return (
-    <footer
-      className={clsx(
-        className,
-      )}
+    <div
+      className="p-10 border-t-2 border-slate-500"
+      style={{
+        background: `linear-gradient(154.09deg, rgba(0, 0, 0, 0.2) 16.35%, rgba(0, 0, 0, 0) 39.32%), radial-gradient(14.08% 17.22% at 86.26% 77.02%, rgba(255, 255, 255, 0.04) 0%, rgba(255, 255, 255, 0) 100%), radial-gradient(31.27% 31.27% at 81.13% 24.31%, rgba(86, 80, 28, 0.2) 0%, rgba(11, 36, 32, 0) 100%), radial-gradient(26.56% 26.56% at 19.78% 70.31%, rgba(54, 22, 28, 0.2) 0%, rgba(10, 14, 27, 0) 100%), linear-gradient(180deg, #2F3A44 0%, #202B2B 100%)`,
+      }}
     >
-      <ul className={clsx(
-        "max-w-4xl px-6 m-auto",
-        "grid grid-cols-3",
-        "[&_h2]:mb-3 [&_h2]:italic [&_h2]:font-extralight [&_h2]:text-white/50"
-      )}>
-        <li>
-          <h2>{i18n.firststep}</h2>
-          <ul>
-            <li>
-              <Link
-                className="text-white/70 hover:text-[#00FFFA]"
-                href={`/${lang}/discover`}
-              >
-                Discover
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="text-white/70 hover:text-[#00FFFA]"
-                href={`/${lang}/learn/getting-started`}
-              >
-                Getting started
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="text-white/70 hover:text-[#00FFFA]"
-                href={`#`}
-              >
-                Download
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="text-white/70 hover:text-[#00FFFA]"
-                href={lang === 'en' ? `/en/discover/sitemap` : `/fr/decouvrir/plan-site`}
-              >
-                Sitemap
-              </Link>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <h2>{i18n.advanced}</h2>
-          <ul>
-            <li>
-              <Link
-                className="text-white/70 hover:text-[#00FFFA]"
-                href={`#`}
-              >
-                Configuration
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="text-white/70 hover:text-[#00FFFA]"
-                href={`#`}
-              >
-                Using the CLI
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="text-white/70 hover:text-[#00FFFA]"
-                href={`#`}
-              >
-                Rest API
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="text-white/70 hover:text-[#00FFFA]"
-                href={`#`}
-              >
-                Advanced usage
-              </Link>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <h2>{i18n.community}</h2>
-          <ul className="space-y-1">
-            <li>
-              <a
-                className="text-white/70 hover:text-[#00FFFA]"
-                href="https://github.com/TOSIT-IO/TDP"
-                target="_blank"
-              >
-                GitHub repository
-              </a>
-            </li>
-            <li>
-              <a
-                className="text-white/70 hover:text-[#00FFFA]"
-                href="https://github.com/TOSIT-IO/TDP/issues"
-                target="_blank"
-              >
-                Issues
-              </a>
-            </li>
-            <li>
-              <a
-                className="text-white/70 hover:text-[#00FFFA]"
-                href="https://github.com/orgs/TOSIT-IO/discussions"
-                target="_blank"
-              >
-                Discussions
-              </a>
-            </li>
-            <li>
-              <a
-                className="text-white/70 hover:text-[#00FFFA]"
-                href="https://github.com/TOSIT-IO/TDP/blob/main/LICENSE"
-                target="_blank"
-              >
-                License
-              </a>
-            </li>
-          </ul>
-        </li>
-      </ul>
-    </footer>
+      <footer
+        className={clsx(
+          className,
+        )}
+      >
+        <ul className={clsx(
+          "max-w-4xl px-6 m-auto",
+          "grid grid-cols-3",
+          "[&_h2]:mb-3 [&_h2]:italic [&_h2]:font-extralight [&_h2]:text-white/50"
+        )}>
+          <li>
+            <h2>{i18n.firststep}</h2>
+            <ul>
+              <li>
+                <Link
+                  className="text-white/70 hover:text-[#00FFFA]"
+                  href={`/${lang}/discover`}
+                >
+                  Discover
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-white/70 hover:text-[#00FFFA]"
+                  href={`/${lang}/learn/getting-started`}
+                >
+                  Getting started
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-white/70 hover:text-[#00FFFA]"
+                  href={`#`}
+                >
+                  Download
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-white/70 hover:text-[#00FFFA]"
+                  href={lang === 'en' ? `/en/discover/sitemap` : `/fr/decouvrir/plan-site`}
+                >
+                  Sitemap
+                </Link>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <h2>{i18n.advanced}</h2>
+            <ul>
+              <li>
+                <Link
+                  className="text-white/70 hover:text-[#00FFFA]"
+                  href={`#`}
+                >
+                  Configuration
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-white/70 hover:text-[#00FFFA]"
+                  href={`#`}
+                >
+                  Using the CLI
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-white/70 hover:text-[#00FFFA]"
+                  href={`#`}
+                >
+                  Rest API
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-white/70 hover:text-[#00FFFA]"
+                  href={`#`}
+                >
+                  Advanced usage
+                </Link>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <h2>{i18n.community}</h2>
+            <ul className="space-y-1">
+              <li>
+                <a
+                  className="text-white/70 hover:text-[#00FFFA]"
+                  href="https://github.com/TOSIT-IO/TDP"
+                  target="_blank"
+                >
+                  GitHub repository
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-white/70 hover:text-[#00FFFA]"
+                  href="https://github.com/TOSIT-IO/TDP/issues"
+                  target="_blank"
+                >
+                  Issues
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-white/70 hover:text-[#00FFFA]"
+                  href="https://github.com/orgs/TOSIT-IO/discussions"
+                  target="_blank"
+                >
+                  Discussions
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-white/70 hover:text-[#00FFFA]"
+                  href="https://github.com/TOSIT-IO/TDP/blob/main/LICENSE"
+                  target="_blank"
+                >
+                  License
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </footer>
+    </div>
   )
 }
--- src/app/[lang]/layout.js
+++ src/app/[lang]/layout.js
@@ -5,14 +5,7 @@ export default async function RootLayout({ children, params }) {
   return (
     <>
       {children}
-      <div
-        className="p-10 border-t-2 border-slate-500"
-        style={{
-          background: `linear-gradient(154.09deg, rgba(0, 0, 0, 0.2) 16.35%, rgba(0, 0, 0, 0) 39.32%), radial-gradient(14.08% 17.22% at 86.26% 77.02%, rgba(255, 255, 255, 0.04) 0%, rgba(255, 255, 255, 0) 100%), radial-gradient(31.27% 31.27% at 81.13% 24.31%, rgba(86, 80, 28, 0.2) 0%, rgba(11, 36, 32, 0) 100%), radial-gradient(26.56% 26.56% at 19.78% 70.31%, rgba(54, 22, 28, 0.2) 0%, rgba(10, 14, 27, 0) 100%), linear-gradient(180deg, #2F3A44 0%, #202B2B 100%)`,
-        }}
-      >
-        <Footer lang={params.lang} />
-      </div>
+      <Footer lang={params.lang} />
     </>
   )
 }
