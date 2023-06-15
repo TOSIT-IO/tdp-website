76693ede7f3221e7f35fa0c27e28a2c7c13fa6a5
David Worms <david@adaltas.com>
2023-06-15 09:41:22 +0200

    refactor(layout): comment unused frontmatter plugins

--- src/mdx/remark.js
+++ src/mdx/remark.js
@@ -1,13 +1,14 @@
 import { mdxAnnotations } from 'mdx-annotations'
-import remarkFrontmatter from 'remark-frontmatter'
+// import remarkFrontmatter from 'remark-frontmatter'
 import remarkGfm from 'remark-gfm'
-import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
+// import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
+import remarkSmartypants from '@davidcelis/remark-smartypants'
 
 const plugins = [
   mdxAnnotations.remark,
-  remarkFrontmatter,
+  // remarkFrontmatter,
   remarkGfm,
-  remarkMdxFrontmatter,
+  // remarkMdxFrontmatter,
   [remarkSmartypants, { dashes: 'oldschool' }],
 ]
 
