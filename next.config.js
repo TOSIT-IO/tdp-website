848f84e57e0995730c4875475d5f0669597113a8
David Worms <david@adaltas.com>
2023-06-16 07:01:43 +0200

    fix(project): disable base path in dev mode

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ import rehype from './src/mdx/rehype.js'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/tdp-website',
+  basePath: process.env.NODE_ENV === 'development' ? undefined : '/tdp-website',
   // Static Exports
   // See https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
   output: 'export',
@@ -63,6 +63,15 @@ const withMDX = mdx({
     // providerImportSource: "@mdx-js/react",
   },
 })
+
+// const withRedac = (config) => {
+//   // console.log(fn)
+//   // return (...args) => {
+//   //   return fn.apply(null, args)
+//   // }
+//   console.log(':withRedac:', config)
+//   return config
+// }
 
 // export default nextConfig
 export default withMDX(nextConfig)
