04350dabbcffa40e663b449f7b9a1a126e58856d
Worms David <david@adaltas.com>
2023-09-27 21:49:12 +0200

    fix(layout): h1 double usage warning (#30)

--- src/mdx/components/index.js
+++ src/mdx/components/index.js
@@ -2,3 +2,12 @@
 export { Code as code, Pre as pre } from '@/mdx/components/Code'
 export { Tabs } from '@/mdx/components/Tabs'
 export { Properties, Property } from '@/mdx/components/Properties'
+
+const h1 = () => (
+  <div>
+    WARNING: heading 1 are handled by the template, only one is accepted at
+    the begining of a page.
+  </div>
+)
+
+export { h1 }
