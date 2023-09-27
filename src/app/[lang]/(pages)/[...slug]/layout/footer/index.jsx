a2260104879fdd65cc994b24ab9761a0cf04d1f5
Worms David <david@adaltas.com>
2023-09-27 22:03:54 +0200

    feat(layout): style footer for small devices (#31)

--- src/app/[lang]/(pages)/[...slug]/layout/footer/index.jsx
+++ src/app/[lang]/(pages)/[...slug]/layout/footer/index.jsx
@@ -22,7 +22,7 @@ export default async function Footer({
     .get()
   return (
     <div
-      className="p-10 border-t-2 border-slate-500"
+      className="border-t-2 border-slate-500"
       style={{
         background: `linear-gradient(154.09deg, rgba(0, 0, 0, 0.2) 16.35%, rgba(0, 0, 0, 0) 39.32%), radial-gradient(14.08% 17.22% at 86.26% 77.02%, rgba(255, 255, 255, 0.04) 0%, rgba(255, 255, 255, 0) 100%), radial-gradient(31.27% 31.27% at 81.13% 24.31%, rgba(86, 80, 28, 0.2) 0%, rgba(11, 36, 32, 0) 100%), radial-gradient(26.56% 26.56% at 19.78% 70.31%, rgba(54, 22, 28, 0.2) 0%, rgba(10, 14, 27, 0) 100%), linear-gradient(180deg, #2F3A44 0%, #202B2B 100%)`,
       }}
@@ -34,7 +34,7 @@ export default async function Footer({
       >
         <ul className={clsx(
           "max-w-4xl px-6 m-auto",
-          "grid grid-cols-3",
+          "grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 gap-10 sm:gap-0 py-10",
           "[&_h2]:mb-3 [&_h2]:italic [&_h2]:font-extralight [&_h2]:text-white/50"
         )}>
           <li>
