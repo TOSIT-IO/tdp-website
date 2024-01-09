849d71d1d0e8d181e641c1ffef052742100b767f
David Worms <david@adaltas.com>
2024-01-09 19:43:27 +0100

    feat(pages): homepage features

---
+++ src/layout/home/features.js
@@ -0,0 +1,9 @@
+import Community from '@/layout/home/features/Community.svg'
+import Core from '@/layout/home/features/Core-1.svg'
+import Deployment from '@/layout/home/features/Deployment.svg'
+import Free from '@/layout/home/features/Free.svg'
+import Independance from '@/layout/home/features/Independance.svg'
+import Open from '@/layout/home/features/Open.svg'
+import Secured from '@/layout/home/features/Secured.svg'
+
+export { Community, Core, Deployment, Free, Independance, Open, Secured }
